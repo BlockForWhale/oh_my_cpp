2870e0d1322fd697ba644a2b395a1a7abfe439ff
jerry <jesse2061@outlook.com>
2018-06-11 16:20:12 +0800

    离散数学第十三周的实验题

--- main.cpp
+++ main.cpp
@@ -1,48 +1,46 @@
 #include <bits/stdc++.h>
 
-using namespace std;
-
-
-const int N = 510;
-
-int color[N], graph[N][N];
-
-bool bfs(int s, int n) {
-    queue<int> q;
-    q.push(s);
-    color[s] = 1;
-    while (!q.empty()) {
-        int from = q.front();
-        q.pop();
-        for (int i = 1; i <= n; i++) {
-            if (graph[from][i] && color[i] == -1) {
-                q.push(i);
-                color[i] = !color[from];
-            }
-            if (graph[from][i] && color[from] == color[i])
-                return false;
-        }
-    }
-    return true;
-}
+using std::cin;
+using std::cout;
+using std::endl;
 
 int main() {
-    int n, m, a, b, i;
-    memset(color, -1, sizeof(color));
+    int e[10][10], dis[10], book[10], i, j, n, m, t1, t2, t3, u, v, min;
+    int maxN = 2147483647;
     cin >> n >> m;
-    for (i = 0; i < m; i++) {
-        cin >> a >> b;
-        graph[a][b] = graph[b][a] = 1;
-    }
-    bool flag = false;
+
     for (i = 1; i <= n; i++)
-        if (color[i] == -1 && !bfs(i, n)) {
-            flag = true;
-            break;
+        for (j = 1; j <= n; j++)
+            if (i == j) e[i][j] = 0;
+            else e[i][j] = maxN;
+    for (i = 1; i <= m; i++) {
+        scanf("%d %d %d", &t1, &t2, &t3);
+        e[t1][t2] = t3;
+    }
+    int start, end;
+    cout << "start:";
+    cin >> start;
+    cout << "end:";
+    cin >> end;
+    for (i = 0; i < n; i++) dis[i + 1] = e[start][i + 1];
+    for (i = 0; i < n; i++) book[i + 1] = 0;
+    book[1] = 1;
+    for (i = 0; i < n - 1; i++) {
+        min = maxN;
+        for (j = 1; j <= n; j++) {
+            if (book[j] == 0 && dis[j] < min) {
+                min = dis[j];
+                u = j;
+            }
         }
-    if (flag)
-        cout << "no" << endl;
-    else
-        cout << "yes" << endl;
+        book[u] = 1;
+        for (v = 1; v <= n; v++) {
+            if (e[u][v] < maxN) {
+                if (dis[v] > dis[u] + e[u][v])
+                    dis[v] = dis[u] + e[u][v];
+            }
+        }
+    }
+    cout << dis[end] << endl;
     return 0;
 }
