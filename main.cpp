90856c33079d3a244e20734dce59cf9b057cb0b8
jerry <jesse2061@outlook.com>
2018-06-13 13:51:49 +0800

    C++ oj 第二题：函数的重载、默认形参练习

--- main.cpp
+++ main.cpp
@@ -3,54 +3,108 @@
 using std::cin;
 using std::cout;
 using std::endl;
+using std::string;
+
+string padLeft(string src, int length, char padding = ' ') {
+    if (length <= src.length()) return src;
+    int len = length - src.length();
+    src.insert(0, len, padding);
+    return src;
+}
+
+string padRight(string src, int length, char padding = ' ') {
+    if (length <= src.length()) return src;
+    int len = length - src.length();
+    src.insert(src.length(), len, padding);
+    return src;
+}
+
+string cpy(string src, int startIndex = 0, int endIndex = 999) {
+    if (endIndex == 999) endIndex = src.length() - 1;
+    if (startIndex >= src.length() || startIndex < 0) return "";
+    if (endIndex >= src.length() || endIndex < startIndex) return "";
+    int j = 0;
+    //for(int i = startIndex; i <= endIndex; i++,j++){
+    //    cout<<src[i];
+    //    src[j] = src[i];
+    //}
+    src = src.substr(startIndex, endIndex - startIndex + 1);
+    //cout<<"done:"<<j<<endl;
+    //src[j] = '\0';
+    //cout<<src<<endl;
+    return src;
+}
+
+string remove(string src, char delChar) {
+    for (int i = 0; i < src.length(); i++) {
+        if (src[i] == delChar) {
+            src = src.erase(i, 1);
+            i--;
+        }
+    }
+    return src;
+}
+
+string remove(string src, int index) {
+    if (index >= src.length() || index < 0) return src;
+    src = src.erase(index, 1);
+    return src;
+}
+
+string remove(string src, int startIndex, int endIndex) {
+    if (startIndex > src.length() || startIndex < 0) return src;
+    if (endIndex > src.length() || endIndex < startIndex) return src;
+    int len = endIndex - startIndex + 1;
+    return src.erase(startIndex, len);
+}
 
 int main() {
-    int n, is = 0;
-    while (cin >> n) {
-        int p[n], cnt[n];
-        for (int i = 0; i < n; i++) {
-            cin >> p[i];
-            cnt[i] = 1;
+    int num, length, index, startIndex, endIndex;
+    char padding, delChar;
+    string src, dest;
+    while (cin >> num) {
+        switch (num) {
+            case 11:
+                cin >> src >> length;
+                cout << padLeft(src, length) << endl;
+                break;
+            case 12:
+                cin >> src >> length >> padding;
+                cout << padLeft(src, length, padding) << endl;
+                break;
+            case 21:
+                cin >> src >> length;
+                cout << padRight(src, length) << endl;
+                break;
+            case 22:
+                cin >> src >> length >> padding;
+                cout << padRight(src, length, padding) << endl;
+                break;
+            case 31:
+                cin >> src;
+                cout << cpy(src) << endl;
+                break;
+            case 32:
+                cin >> src >> startIndex;
+                cout << cpy(src, startIndex) << endl;
+                break;
+            case 33:
+                cin >> src >> startIndex >> endIndex;
+                cout << cpy(src, startIndex, endIndex) << endl;
+                break;
+            case 41:
+                cin >> src >> delChar;
+                cout << remove(src, delChar) << endl;
+                break;
+            case 42:
+                cin >> src >> index;
+                cout << remove(src, index) << endl;
+                break;
+            case 43:
+                cin >> src >> startIndex >> endIndex;
+                cout << remove(src, startIndex, endIndex) << endl;
+                break;
         }
-        for (int i = 0; i < n; i++) {
-            if (i == 0) {
-                if (p[i] > p[i + 1] && i != n - 1 && cnt[i] <= cnt[i + 1])
-                    cnt[i] = cnt[i + 1] + 1;
-            } else if (i == n - 1) {
-                if (p[i] > p[i - 1] && cnt[i] <= cnt[i - 1])
-                    cnt[i] = cnt[i - 1] + 1;
-                if (p[i - 1] > p[i] && cnt[i - 1] <= cnt[i])
-                    cnt[i - 1] = cnt[i] + 1;
-            } else {
-                if (p[i] > p[i - 1] && cnt[i] <= cnt[i - 1])
-                    cnt[i] = cnt[i - 1] + 1;
-                if (p[i] > p[i + 1] && cnt[i] <= cnt[i + 1])
-                    cnt[i] = cnt[i + 1] + 1;
-                if (p[i - 1] > p[i] && cnt[i - 1] <= cnt[i])
-                    cnt[i - 1] = cnt[i] + 1;
-            }
-        }
-        for (int i = n - 1; i >= 0; i--) {
-            if (i == 0) {
-                if (p[i] > p[i + 1] && i != n - 1 && cnt[i] <= cnt[i + 1])
-                    cnt[i] = cnt[i + 1] + 1;
-            } else if (i == n - 1) {
-                if (p[i] > p[i - 1] && cnt[i] <= cnt[i - 1])
-                    cnt[i] = cnt[i - 1] + 1;
-                if (p[i - 1] > p[i] && cnt[i - 1] <= cnt[i])
-                    cnt[i - 1] = cnt[i] + 1;
-            } else {
-                if (p[i] > p[i - 1] && cnt[i] <= cnt[i - 1])
-                    cnt[i] = cnt[i - 1] + 1;
-                if (p[i] > p[i + 1] && cnt[i] <= cnt[i + 1])
-                    cnt[i] = cnt[i + 1] + 1;
-                if (p[i - 1] > p[i] && cnt[i - 1] <= cnt[i])
-                    cnt[i - 1] = cnt[i] + 1;
-            }
-        }
-        int total = 0;
-        for (int i = 0; i < n; i++) total += cnt[i];
-        cout << total << endl;
     }
     return 0;
 }
