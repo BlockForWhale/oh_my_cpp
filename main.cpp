63125b4903beee44ffd2e993d2c0deb5d99e39ba
jerry <jesse2061@outlook.com>
2018-06-13 15:30:15 +0800

    C++ oj 第11题：盒子类

--- main.cpp
+++ main.cpp
@@ -2,29 +2,42 @@
 
 using namespace std;
 
-int myatoi(char *string) {
-    int num = 0;
-    int s = '+';
-    if (*string == '-') {
-        s = '-';
-        string++;
+class Box {
+private:
+    int Length;
+    int Width;
+    int Height;
+    int V;
+    int S;
+public:
+    void initBox(int x, int y, int z) {//设置Box的尺寸
+        Length = x;
+        Width = y;
+        Height = z;
     }
-    if (*string == '+') {
-        s = '+';
-        string++;
+
+    void setVolume() {//计算Box的体积
+        V = Length * Width * Height;
     }
-    while (*string >= '0' && *string <= '9') {
-        if (*string == 0) break;
-        num = num * 10 + (*string - '0');
-        string++;
+
+    void setArea() { //计算Box的表面积
+        S = Length * Width * 2 + Length * Height * 2 + Width * Height * 2;
     }
-    int r = (s == '+' ? 1 : -1) * num;
-    return r;
-}
+
+    void show() {//输出盒子的信息，输出形式见“输出说明”
+        cout << Length << ' ' << Width << ' ' << Height << endl << V << endl << S << endl;
+    }
+};
 
 int main() {
-    char str[100];
-    while (cin >> str)
-        cout << myatoi(str) << endl;
+    int intLength, intWidth, intHeight;
+    Box box;
+    cin >> intLength;
+    cin >> intWidth;
+    cin >> intHeight;
+    box.initBox(intLength, intWidth, intHeight);
+    box.setVolume();
+    box.setArea();
+    box.show();
     return 0;
 }
