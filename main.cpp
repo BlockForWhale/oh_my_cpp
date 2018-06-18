7c0f3844172088508d287a9a575dba48f1ad27cb
jerry <jesse2061@outlook.com>
2018-06-19 00:45:30 +0800

    C++ oj 第15题：分数计算

--- main.cpp
+++ main.cpp
@@ -2,72 +2,27 @@
 
 using namespace std;
 
-class Date {
-    int year;
-    int month;
-    int day;
-public:
-    Date(int y, int m, int d) : year(y), month(m), day(d) {};
-
-    int getYear() const { return year; }
-
-    int getMonth() const { return month; }
-
-    int getDay() const { return day; }
-};
-
-int getDaysOfMonth(int y, int m) {
-    switch (m) {
-        case 1:
-        case 3:
-        case 5:
-        case 7:
-        case 8:
-        case 10:
-        case 12:
-            return 31;
-        case 4:
-        case 6:
-        case 9:
-        case 11:
-            return 30;
-        case 2:
-        default:
-            return y % 4 != 0 ? 28 : (y % 100 != 0 ? 29 : (y % 400 != 0 ? 28 : 29));
-    }
-}
-
-int calDay(const Date &d1, const Date &d2) {
-    if (d1.getYear() < d2.getYear())
-        return calDay(d2, d1);
-    if (d1.getYear() == d2.getYear() && d1.getMonth() < d2.getMonth())
-        return calDay(d2, d1);
-    if (d1.getYear() == d2.getYear() && d1.getMonth() == d2.getMonth() && d1.getDay() < d2.getDay())
-        return calDay(d2, d1);
-    int sum = 0, y = d2.getYear(), m = d2.getMonth(), d = d2.getDay();
-    while (d1.getYear() != y || d1.getMonth() != m || d1.getDay() != d) {
-        d++;
-        if (d > getDaysOfMonth(y, m)) {
-            m++;
-            d = 1;
+void CalScore(string str[], int num) {
+    for (int i = 0; i < num; i++) {
+        int score = 0;
+        int addPlus = 1;
+        for (int j = 0; j < str[i].length(); j++) {
+            if (str[i][j] == 'O') {
+                score += addPlus++;
+            } else {
+                addPlus = 1;
+            }
         }
-        if (m > 12) {
-            y++;
-            m = 1;
-        }
-        ++sum;
+        cout << score << endl;
     }
-    return sum;
 }
 
 int main() {
-    int y1, m1, d1, y2, m2, d2;
-    cin >> y1 >> m1 >> d1;
-    cin >> y2 >> m2 >> d2;
-    Date date1(y1, m1, d1);
-    Date date2(y2, m2, d2);
-    cout << calDay(date1, date2);
-
+    int i, num;
+    cin >> num;
+    string str[num];
+    for (i = 0; i < num; i++)
+        cin >> str[i];
+    CalScore(str, num);  //要求自行设计
     return 0;
-
 }
