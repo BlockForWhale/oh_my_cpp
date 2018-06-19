614f47bc414a2cb843f96e4482489cadfbcac614
jerry <jesse2061@outlook.com>
2018-06-19 19:42:35 +0800

    C++ oj 第24题：某年龄段内的学生

--- main.cpp
+++ main.cpp
@@ -1,66 +1,123 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
-class Rectangle;
-
-class Point {
+class Date {
 private:
-    int x, y;
+    int year;
+    int month;
+    int day;
 public:
-    Point(int x, int y) {
-        this->x = x;
-        this->y = y;
+    static int maxYear;
+    static int minYear;
+
+    Date(int y, int m, int d) : year(y), month(m), day(d) {}
+
+    static void SetMaxYear(int maxY) {
+        maxYear = maxY;
     }
 
-    friend class Rectangle;
+    static void SetMinYear(int minY) {
+        minYear = minY;
+    }
+
+    friend class StudentList;
+
+    friend class Student;
 };
 
-class Rectangle {
+int Date::maxYear = 0;
+int Date::minYear = 0;
+
+class Student {
 private:
-    Point topLeft, topRight, bottomLeft, bottomRight;
-
-    bool isRectangle() {
-        return topLeft.x == bottomLeft.x && topLeft.y == topRight.y && bottomLeft.y == bottomRight.y &&
-               topRight.x == bottomRight.x;
-    }
-
+    int num;
+    string name;
+    Date birthday;
 public:
-    Rectangle(Point stopLeft, Point stopRight, Point sbottomLeft, Point sbottomRight) : topLeft(stopLeft),
-                                                                                        topRight(stopRight),
-                                                                                        bottomLeft(sbottomLeft),
-                                                                                        bottomRight(sbottomRight) {}
 
-    int getArea() {
-        if (!isRectangle()) return 0;
-        int x = bottomRight.x - topLeft.x;
-        int y = bottomRight.y - topLeft.y;
-        if (x < 0) x = 0 - x;
-        if (y < 0) y = 0 - y;
-        return x * y;
+    Student() : num(-1), name(""), birthday(Date(0, 0, 0)) {}
+
+    Student(int nums, string names, Date birthdays) : num(nums), name(names), birthday(birthdays) {
+        if (birthdays.year > Date::maxYear) birthdays.year = Date::maxYear;
+        if (birthdays.year < Date::minYear) birthdays.year = Date::minYear;
+        this->birthday = birthdays;
     }
 
-    bool isIn(Point p) {
-        if (!isRectangle()) return false;
-        return p.x > topLeft.x && p.x < bottomRight.x && p.y >= topLeft.y && p.y < bottomRight.y;
+    friend class StudentList;
+};
+
+class StudentList {
+private:
+    Student list[100];
+    int count;
+public:
+    StudentList() : count(0) {}
+
+    void AddStudent(Student stu) {
+        list[count++] = stu;
+    }
+
+    void DeleteStudent(int num) {
+        for (int i = 0; i < count; i++) {
+            if (list[i].num == num) {
+                for (int j = i; j < count - 1; j++) {
+                    list[j] = list[j + 1];
+                }
+                count--;
+            }
+        }
+    }
+
+    StudentList GetStudent(int year1, int year2) {
+        StudentList ls;
+        for (int i = 0; i < count; i++) {
+            if (list[i].birthday.year >= year1 && list[i].birthday.year <= year2) {
+                ls.AddStudent(list[i]);
+            }
+        }
+        return ls;
+    }
+
+    void Print() {
+        if (count == 0)
+            cout << "No result." << endl;
+        else {
+            for (int i = 0; i < count; i++) {
+                cout << list[i].num << ' ' << list[i].name << ' ' << list[i].birthday.year << ' '
+                     << list[i].birthday.month << ' ' << list[i].birthday.day << endl;
+            }
+        }
     }
 };
 
 int main() {
-    int topLeftX, topLeftY, topRightX, topRightY, bottomLeftX, bottomLeftY, bottomRightX, bottomRightY;
-    int px, py;
-
-    cin >> topLeftX >> topLeftY >> topRightX >> topRightY >> bottomLeftX >> bottomLeftY >> bottomRightX >> bottomRightY;
-    cin >> px >> py;
-
-    Point p(px, py);
-    Rectangle r(Point(topLeftX, topLeftY), Point(topRightX, topRightY), Point(bottomLeftX, bottomLeftY),
-                Point(bottomRightX, bottomRightY));
-    cout << r.getArea() << endl;
-    if (r.isIn(p))
-        cout << "In" << endl;
-    else
-        cout << "Not in" << endl;
-
+    int num, birthYear, birthMonth, birthDay;
+    int maxYear, minYear, year1, year2;
+    int op;
+    string name;
+    StudentList sl;
+    Student stu;
+    cin >> maxYear >> minYear;
+    Date::SetMaxYear(maxYear);
+    Date::SetMinYear(minYear);
+    while (cin >> op) {
+        switch (op) {
+            case 1:
+                cin >> num >> name >> birthYear >> birthMonth >> birthDay;
+                stu = Student(num, name, Date(birthYear, birthMonth, birthDay));
+                sl.AddStudent(stu);
+                break;
+            case 2:
+                cin >> num;
+                sl.DeleteStudent(num);
+                break;
+            case 3:
+                cin >> year1 >> year2;
+                StudentList temp = sl.GetStudent(year1, year2);
+                temp.Print();
+                break;
+        }
+    }
     return 0;
 }
