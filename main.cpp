f6a659bfeaa8c47d067452d7443cbcad5eebee29
jerry <jesse2061@outlook.com>
2018-06-19 21:06:27 +0800

    C++ oj 第26题：查找学生

--- main.cpp
+++ main.cpp
@@ -1,121 +1,89 @@
 #include <bits/stdc++.h>
+#include <utility>
 
 using namespace std;
 
-class MyString {
-public:
-    static int totalCount;
-
-    MyString(unsigned n, char c) {
-        totalCount++;
-        s = "";
-        for (int i = 0; i < n; i++) {
-            s = s + c;
-        }
-    }
-
-    MyString(char *p) {
-        s = p;
-        totalCount++;
-    }
-
-    MyString() {
-        s = "";
-        totalCount++;
-    }
-
-    MyString(const MyString &r) {
-        s = r.s;
-        totalCount++;
-    }
-
-    MyString &operator=(MyString r) {
-        s = r.s;
-        return *this;
-    }
-
-    ~MyString() {
-        totalCount--;
-    }
-
-    static int GetCount() {
-        return totalCount;
-    }
-
-    void ShowStr() {
-        cout << s;
-    }
-
+class Student {
 private:
-    string s;
+    int number;//学号
+    string name;//名字
+    double chinese;//语文成绩
+    double math;//数学成绩
+    double average;//平均成绩
+public:
+    Student() {}
+
+    Student(int numbers, string names) : number(numbers), name(names) {}
+
+    bool setChinese(double num) {
+        if (num > 0 && num <= 100) {
+            chinese = num;
+            return true;
+        }
+        chinese = 0;
+        return false;
+    }
+
+    bool setMath(double num) {
+        if (num >= 0 && num <= 100) {
+            math = num;
+            return true;
+        }
+        math = 0;
+        return false;
+    }
+
+    double getChinese() { return chinese; }
+
+    double getMath() { return math; }
+
+    int getNumber() { return number; }
+
+    string getName() { return name; }
+
+    double getAverage() {
+        average = math + chinese;
+        average = average / 2.0;
+        return average;
+    }
 };
 
-int MyString::totalCount = 0;
-
-void fun1(int n) {
-    //cout << "定义数组前：" << MyString::GetCount() << endl;
-    MyString strArr[n];
-    //cout << "定义数组后：" << MyString::GetCount() << endl;
-    MyString *pStr;
-    cout << strArr[0].GetCount() << endl;
-}
-
-void fun2(int n) {
-    //cout << "定义数组前：" << MyString::GetCount() << endl;
-    MyString *pStr = new MyString[n];
-    //cout << "定义数组后：" << MyString::GetCount() << endl;
-    cout << MyString::GetCount() << endl;
-}
-
-void fun3(MyString &s1, MyString s2) {
-    MyString s3;
-    s3 = s1 = s2;
-    cout << MyString::GetCount() << endl;
-    s3.ShowStr();
-    cout << endl;
+void findStudent(Student stu[], int n, string search) {
+    bool finds = false;
+    for (int i = 0; i < n; i++) {
+        if (stu[i].getName().find(search, 0) != stu[i].getName().npos) {
+            finds = true;
+            cout << stu[i].getNumber()
+                 << ' '
+                 << stu[i].getName()
+                 << ' '
+                 << stu[i].getChinese()
+                 << ' '
+                 << stu[i].getMath()
+                 << ' '
+                 << stu[i].getAverage()
+                 << endl;
+        }
+    }
+    if (!finds) {
+        cout << "Not found." << endl;
+    }
 }
 
 int main() {
-    //cout << "initial：" << MyString::GetCount() << endl;
-    MyString s1;
-    MyString s2;
-    int n;
-    char charArr[20];
-    int op;
-    while (cin >> op) {
-        switch (op) {
-            case 1: {
-                cin >> n;
-                fun1(n);
-                break;
-            }
-            case 2: {
-                cin >> n;
-                fun2(n);
-                break;
-            }
-            case 3: {
-                int m;
-                char ch;
-                cin >> m >> ch;
-                s2 = MyString(m, ch);
-                fun3(s1, s2);
-                s1.ShowStr();
-
-                cout << endl;
-
-                break;
-            }
-            case 4: {
-                cin >> charArr;
-                s1 = MyString(charArr);
-                cout << MyString::GetCount() << endl;
-                s1.ShowStr();
-                cout << endl;
-                break;
-            }
-        }
+    int n, i;
+    int number;
+    string name, searchName;
+    double math, chinese;
+    cin >> n;
+    Student stu[n];
+    for (i = 0; i < n; i++) {
+        cin >> number >> name >> chinese >> math;
+        stu[i] = Student(number, name);
+        stu[i].setChinese(chinese);
+        stu[i].setMath(math);
     }
-
+    cin >> searchName;  //输入需要查找的姓名
+    findStudent(stu, n, searchName);  //调用函数，查找学生，输出信息
     return 0;
 }
