34101451904ffc86bd48306f66c2b37a729d89ea
jerry <jesse2061@outlook.com>
2018-06-19 22:43:17 +0800

    C++ oj 第29题：身份证类(对象数组)

--- main.cpp
+++ main.cpp
@@ -2,93 +2,92 @@
 
 using namespace std;
 
-#define tab '\t'
-
-class Array {
+class ID {
 private:
-    int **array;
+    char *id;
+    int year, month, date;
+    char sex, youth;
 public:
-    Array(int **t, int m, int n) {//构造函数，用二阶指针t初始化二阶指针array关联访问的成员数组，m和n分别对应二维数组的行数、列数
-        array = new int *[m];
-        for (int i = 0; i < m; i++) {
-            array[i] = new int[n];
-            for (int j = 0; j < n; j++)
-                array[i][j] = t[i][j];
-        }
+    void set_ID(char *s) {
+        char *p = (char *) malloc(sizeof(*s) * (strlen(s) + 1));
+        strcpy(p, s);
+        id = p;
+        //cout << "current ID(first): "<<id<<endl;
+        year = 0;
+        month = 0;
+        date = 0;
+        sex = 'F';
+        youth = 'N';
     }
 
-    int comp(int n) {//判断整数n是否为合数，如果是合数，返回值为1，否则返回值为0
-        for (int i = 2; i < n; i++)
-            if (n % i == 0) return 1;
-        return 0;
+    int getAge() {
+        int age = 2015 - year;
+        int asd = month * 1000 + date;
+        if (asd > 1001) age--;
+        return age;
     }
 
-    int sum_comp(int j) {//求数组a第j列元素中所有合数之和
-        int result = 0;
-        for (int i = 0; i < 4; i++) {
-            if (comp(array[i][j]) == 1) result += array[i][j];
-        }
-        return result;
+    void fun() {
+        char *start = id;
+        //cout << "current ID: "<<id<<endl;
+        for (int i = 0; i < 6; i++) id++;
+        //cout << "current ID: "<<id<<endl;
+        year += ((*id - '0') * 1000);
+        id++;
+        //cout << "current ID: "<<id<<endl;
+        year += ((*id - '0') * 100);
+        id++;
+        //cout << "current ID: "<<id<<endl;
+        year += ((*id - '0') * 10);
+        id++;
+        //cout << "current ID: "<<id<<endl;
+        year += (*id - '0');
+        id++;
+        //cout << "current ID: "<<id<<endl;
+        month += (*id - '0') * 10;
+        id++;
+        //cout << "current ID: "<<id<<endl;
+        month += (*id - '0');
+        id++;
+        //cout << "current ID: "<<id<<endl;
+        date += (*id - '0') * 10;
+        id++;
+        //cout << "current ID: "<<id<<endl;
+        date += (*id - '0');
+        id += 3;
+        //cout << "current ID: "<<id<<endl;
+        if ((*id - '0') % 2 != 0) sex = 'M';
+        else sex = 'F';
+        id = start;
+        //cout << "current ID: "<<id<<endl;
+        if (sex == 'M' && getAge() <= 35) youth = 'Y';
+        else if (sex == 'M' && getAge() > 35) youth = 'N';
+        else if (sex == 'F' && getAge() <= 40) youth = 'Y';
+        else youth = 'N';
     }
 
-    void exch(int j1, int j2) {//交换成员数组的第j1, j2列元素
-        for (int i = 0; i < 4; i++) {
-            int temp = array[i][j1];
-            array[i][j1] = array[i][j2];
-            array[i][j2] = temp;
-        }
+    void print() {
+        cout << id << endl;
+        cout << year << '.' << month << '.' << date << endl;
+        cout << sex << endl << youth << endl;
     }
 
-    void fun(int m, int n) {//根据题意对成员数组进行列排序，m和n分别对应二维数组的行数、列数
-        for (int i = 0; i < m; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (sum_comp(j) > sum_comp(i)) exch(i, j);
-            }
-        }
+    ~ID() {
+        free(id);
     }
-
-    void print(int m, int n) {//以矩阵的形式输出成员数组，m和n分别对应二维数组的行数、列数
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                cout << array[i][j] << tab;
-            }
-            cout << endl;
-        }
-    }
-
-
 };
 
-bool make2DArray(int row, int col, int **&array);
-
-bool InputEdge(int row, int col, int **&array) {
-    if (make2DArray(row, col, array)) {
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                cin >> array[i][j];
-            }
-        }
-    }
-    return true;
-}
-
-bool make2DArray(int row, int col, int **&array) {
-    array = new int *[row];
-    for (int i = 0; i < row; i++) {
-        array[i] = new int[col];
-    }
-    return true;
-}
-
 int main() {
-    int **array;   //二阶指针，用于关联访问二维数组
-    int m, n;  //m：行数 n：列数
-    cin >> m; //输入行数
-    cin >> n; //输入列数
-    InputEdge(m, n, array); //在申请指定行、列的二维数组的存储空间成功的基础上，针对该二维数组进行初始化
-    Array arr(array, m, n);
-    arr.fun(m, n);
-    arr.print(m, n); //输出排序后的数组
+    int T, i;
+    char id[19];
+    cin >> T;
+    ID person[T];
+    for (i = 0; i < T; i++) {
+        if (i > 0 && i < T) cout << endl;
+        cin >> id;
+        person[i].set_ID(id);
+        person[i].fun();
+        person[i].print();
+    }
     return 0;
-
 }
