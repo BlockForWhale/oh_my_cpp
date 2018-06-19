4c4488898ebc64352005ee57d70e4a264ad32562
jerry <jesse2061@outlook.com>
2018-06-19 16:02:28 +0800

    C++ oj 第20题：学生成绩转换（友元函数）

--- main.cpp
+++ main.cpp
@@ -4,47 +4,43 @@ using namespace std;
 
 class Student {
 public:
-    void Set_StuNum(int x) {
-        stu_num = x;
-    } //自行设计
+    void Set_StuNum(int); //自行设计
+    int Get_StudNum();  //自行设计
+    void Set_Score(float); //自行设计
+    float Get_Score();  //自行设计
+    friend void Score_Trans(Student &s);
 
-    int Get_StudNum() {
-        return stu_num;
-    }  //自行设计
-
-    void Set_Score(float x) {
-        score = x;
-    } //自行设计
-
-    float Get_Score() {
-        return score;
-    }  //自行设计
 private:
     int stu_num; //学号
     float score; //分数
 };
 
-void max(Student *list, int count, int &max_num, float &max_score) {
-    max_score = -2147483647;
-    for (int i = 0; i < count; i++) {
-        if (list[i].Get_Score() > max_score) {
-            max_num = list[i].Get_StudNum();
-            max_score = list[i].Get_Score();
-        }
-    }
+void Student::Set_StuNum(int x) { stu_num = x; }
+
+int Student::Get_StudNum() { return stu_num; }
+
+void Student::Set_Score(float x) { score = x; }
+
+float Student::Get_Score() { return score; }
+
+void Score_Trans(Student &s) {
+    cout << s.stu_num << ' ';
+    cout << (s.score >= 90 ? "优" : (s.score >= 80 ? "良" : (s.score >= 70 ? "中" : (s.score >= 60 ? "及格" : "不及格"))));
 }
 
 int main() {
-    int n, stu_num, max_stu_num;
+    int n, i, stu_num, max_stu_num;
     float score, max_score;
     cin >> n;
     Student stu[n];
-    for (int i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) {
         cin >> stu_num >> score;
         stu[i].Set_StuNum(stu_num);
         stu[i].Set_Score(score);
     }
-    max(stu, n, max_stu_num, max_score);
-    cout << max_stu_num << " " << max_score;
+    for (i = 0; i < n; i++) {
+        Score_Trans(stu[i]);
+        if (i < n - 1) cout << endl;
+    }
     return 0;
 }
