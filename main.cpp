394e61affddb3575222560d0ea85405330bab6e2
jerry <jesse2061@outlook.com>
2018-06-19 16:52:10 +0800

    C++ oj 第23题：长方形?

--- main.cpp
+++ main.cpp
@@ -2,19 +2,65 @@
 
 using namespace std;
 
-template<class T>
-T distance(T x1, T y1, T x2, T y2) {
-    T dis;
-    dis = sqrt((double) (pow(x1 - x2, 2) + pow(y1 - y2, 2)));
-    return dis;
-}
+class Rectangle;
+
+class Point {
+private:
+    int x, y;
+public:
+    Point(int x, int y) {
+        this->x = x;
+        this->y = y;
+    }
+
+    friend class Rectangle;
+};
+
+class Rectangle {
+private:
+    Point topLeft, topRight, bottomLeft, bottomRight;
+
+    bool isRectangle() {
+        return topLeft.x == bottomLeft.x && topLeft.y == topRight.y && bottomLeft.y == bottomRight.y &&
+               topRight.x == bottomRight.x;
+    }
+
+public:
+    Rectangle(Point stopLeft, Point stopRight, Point sbottomLeft, Point sbottomRight) : topLeft(stopLeft),
+                                                                                        topRight(stopRight),
+                                                                                        bottomLeft(sbottomLeft),
+                                                                                        bottomRight(sbottomRight) {}
+
+    int getArea() {
+        if (!isRectangle()) return 0;
+        int x = bottomRight.x - topLeft.x;
+        int y = bottomRight.y - topLeft.y;
+        if (x < 0) x = 0 - x;
+        if (y < 0) y = 0 - y;
+        return x * y;
+    }
+
+    bool isIn(Point p) {
+        if (!isRectangle()) return false;
+        return p.x > topLeft.x && p.x < bottomRight.x && p.y >= topLeft.y && p.y < bottomRight.y;
+    }
+};
 
 int main() {
-    int i_x1, i_y1, i_x2, i_y2;
-    double d_x1, d_y1, d_x2, d_y2;
-    cin >> i_x1 >> i_y1 >> i_x2 >> i_y2;
-    cin >> d_x1 >> d_y1 >> d_x2 >> d_y2;
-    cout << distance(i_x1, i_y1, i_x2, i_y2) << endl;
-    cout << distance(d_x1, d_y1, d_x2, d_y2) << endl;
+    int topLeftX, topLeftY, topRightX, topRightY, bottomLeftX, bottomLeftY, bottomRightX, bottomRightY;
+    int px, py;
+
+    cin >> topLeftX >> topLeftY >> topRightX >> topRightY >> bottomLeftX >> bottomLeftY >> bottomRightX >> bottomRightY;
+    cin >> px >> py;
+
+    Point p(px, py);
+    Rectangle r(Point(topLeftX, topLeftY), Point(topRightX, topRightY), Point(bottomLeftX, bottomLeftY),
+                Point(bottomRightX, bottomRightY));
+    cout << r.getArea() << endl;
+    if (r.isIn(p))
+        cout << "In" << endl;
+    else
+        cout << "Not in" << endl;
+
     return 0;
 }
