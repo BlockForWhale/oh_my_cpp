f30f27139a5ce12c3d69c43b9f2c1b120d009e9c
jerry <jesse2061@outlook.com>
2018-11-20 23:56:39 +0800

    fxy的OJ，数据结构算法练习_队列_4

--- main1/main.cpp
+++ main1/main.cpp
@@ -1,117 +1,157 @@
 #include <iostream>
 
 using namespace std;
-
 #define tmpl template<class ElemType>
-#define FARMER 8
-#define GOAT 1
-#define CABBAGE 2
-#define WOLF 4
+#define QNode LinkQueueNode<ElemType>
 
 tmpl
-class SqQueue {
-private:
-    ElemType *elem;
-    int front;
-    int rear;
-    int maxSize;
-public:
-    SqQueue(int m = 20);
+struct LinkQueueNode {
 
-    bool QueueClear();
+    ElemType data;
 
-    bool QueueisEmpty() const { return front == rear; }
+    QNode *next;
 
-    bool QueueFull() const;
+    LinkQueueNode(QNode *ptr = NULL) { next = ptr; }
 
-    bool GetFront(ElemType &e);
+    LinkQueueNode(const ElemType &item, QNode *ptr = NULL) {
+        next = ptr;
+        data = item;
+    }
 
-    bool enQueue(const ElemType &e);
+    ElemType getData() { return data; }
 
-    bool deQueue(ElemType &e);
+    void SetLink(QNode *link) { next = link; }
 
-    bool QueueDestroy();
-
-    bool DoubleSpace();
+    void SetData(ElemType value) { data = value; }
 };
 
 tmpl
-SqQueue<ElemType>::SqQueue(int m) {
-    if (m == 0) m = 100;
-    elem = new ElemType[m];
-    if (!elem) return;
-    rear = 0;
-    front = rear;
-    maxSize = m;
+class LinkQueue {
+private:
+    QNode *front;
+    QNode *rear;
+    int length;
+public:
+    LinkQueue();
+
+    bool LinkQueueDestroy();
+
+    bool LinkQueueClear();
+
+    int QueueLength() const { return length; }
+
+    bool QueueisEmpty() const;
+
+    bool deQueue(ElemType &e);
+
+    bool enQueue(ElemType e);
+
+    QNode *GetFront() { return front; }
+
+    ElemType GetFrontData() { return front->data; }
+
+    QNode *GetRear() { return rear; }
+
+    bool QueueTraverse() const;
+};
+
+
+
+tmpl
+LinkQueue<ElemType>::LinkQueue() {
+    rear = new QNode;
+    if (!rear) return;
+    else front = rear;
+    front->next = NULL;
+    length = 0;
 }
 
 tmpl
-bool SqQueue<ElemType>::GetFront(ElemType &e) {
+bool LinkQueue<ElemType>::QueueisEmpty() const { return front == rear; }
+
+
+tmpl
+bool LinkQueue<ElemType>::deQueue(ElemType &e) {
 
     if (front == rear) return false;
-    e = elem[(front + 1) % maxSize];
-    return true;
-}
 
-tmpl
-bool SqQueue<ElemType>::DoubleSpace() {
-    ElemType *tmp;
-    tmp = elem;
-    elem = new ElemType[maxSize * 2];
-    if (!elem) return false;
-    for (int i = 0; i < maxSize; i++) elem[i] = tmp[(front + i) % maxSize];
-    front = 0;
-    rear = maxSize;
-    maxSize = maxSize * 2;
-    delete[]tmp;
-    return true;
-}
+    QNode *p;
 
-tmpl
-bool SqQueue<ElemType>::enQueue(const ElemType &e) {
-    if ((rear + 1) % maxSize == front) DoubleSpace();
-    rear = (rear + 1) % maxSize;
-    elem[rear] = e;
+    p = front->next;
+    e = p->data;
+
+    front->next = p->next;
+    length--;
+
+    if (rear == p) rear = front;
+
+    delete (p);
     return true;
 }
 
 
 tmpl
-bool SqQueue<ElemType>::deQueue(ElemType &e) {
-    if (front == rear) return false;
-    front = (front + 1) % maxSize;
-    e = elem[front];
+bool LinkQueue<ElemType>::enQueue(ElemType e) {
+    QNode *s;
+    s = new QNode;
+    s->data = e;
+    s->next = NULL;
+    rear->next = s;
+    rear = s;
+    length = length + 1;
     return true;
 }
 
-template<class ElemType>
-void decode(SqQueue<ElemType> &S, string &code) {
-    int len = code.size();
-    int i = 1, j;
-    char c;
-    ElemType e;
-
-
-    for (int i = 0; i < len; i++) {
-        if (code[i] == ' ') continue;
-        S.enQueue(code[i]);
-    }
-
-    while (!S.QueueisEmpty()) {
-        S.deQueue(e);
-        cout << e;
-        if (!S.QueueisEmpty()) {
-            S.deQueue(e);
-            S.enQueue(e);
+tmpl
+void queuetraining(LinkQueue<ElemType> &lq, int im) {
+    int num;
+    int la, mine1;
+    int n;
+    while (im--) {
+        cin >> n;
+        la = n;
+        mine1 = n;
+        if (n <= 3) {
+            if (n == 1)
+                cout << "1";
+            if (n == 2)
+                cout << "1,2";
+            if (n == 3)
+                cout << "1,2,3";
+        } else {
+            for (int i = 1; i <= n; i++) lq.enQueue(i);
+            while (la > 3) {
+                for (int i = 1; i <= la; i++) {
+                    lq.deQueue(num);
+                    if (i % 2 == 1) {
+                        lq.enQueue(num);
+                    } else --mine1;
+                }
+                la = mine1;
+                if (la > 3) {
+                    for (int i = 1; i <= la; i++) {
+                        lq.deQueue(num);
+                        if (i % 3 != 0) {
+                            lq.enQueue(num);
+                        } else mine1--;
+                    }
+                    la = mine1;
+                }
+            }
+            while (!lq.QueueisEmpty()) {
+                lq.deQueue(num);
+                if (!lq.QueueisEmpty()) cout << num << ",";
+                else cout << num;
+            }
+            if (im > 0) cout << endl;
         }
     }
 }
 
 int main() {
-    SqQueue<char> su;
-    string s;
-    getline(cin, s);
-    decode(su, s);
+    LinkQueue<int> mine;
+    int num;
+    cin >> num;
+    queuetraining(mine, num);
     return 0;
 }
-
