77cc504e8e2b211bdc551f7ae31cd496270efde5
jerry <jesse2061@outlook.com>
2018-11-20 23:42:59 +0800

    fxy的OJ，数据结构算法练习_线性表_13

--- main1/main.cpp
+++ main1/main.cpp
@@ -1,151 +1,124 @@
 #include <iostream>
 
 using namespace std;
+
 #define tmp template<class ElemType>
 #define Node LinkNode<ElemType>
 
-tmp
+template<class ElemType>
 struct LinkNode {
     ElemType data;
-    Node *next;
+    LinkNode<ElemType> *next;
 
-    LinkNode(Node *ptr = NULL) { next = ptr; }
-
-    LinkNode(const ElemType &item, Node *ptr = NULL) {
+    LinkNode(LinkNode<ElemType> *ptr = NULL) { next = ptr; } //构造函数1，用于构造头结点
+    LinkNode(const ElemType &item, LinkNode<ElemType> *ptr = NULL) {
         next = ptr;
         data = item;
     }
+
+    ElemType getData() { return data; }   //取得结点的数据域的值
+    void SetLink(LinkNode<ElemType> *link) { next = link; }  //修改结点的next域
+    void SetLink(ElemType value) { data = value; }   //修改结点的next域
 };
 
-//带头结点的单链表
+//带头结点的循环单链表
 tmp
-class LinkList {
+class CirLinkList {
 private:
     LinkNode<ElemType> *head;   // 头结点
+    LinkNode<ElemType> *tail;   // 尾结点
+    //LinkNode<ElemType> *cur;   // 当前结点
 public:
     //无参数的构造函数
-    LinkList() { head = new LinkNode<ElemType>; }
+    CirLinkList() {
+        head = new LinkNode<ElemType>;
+        tail = head;
+        head->next = head;
+    }
 
     //带参数的构造函数
-    LinkList(const ElemType &item) { head = new LinkNode<ElemType>(item); }
+    CirLinkList(const ElemType &item) {
+        head = new LinkNode<ElemType>(item);
+        tail = head;
+        head->next = head;
+    }
 
-    LinkNode<ElemType> *GetHead() const { return head; }
+    //拷贝构造函数
+    CirLinkList(CirLinkList<ElemType> &List);
 
-    void CreateList_Head(int n, ElemType *A);
+    //在首节点之前插入一个结点
+    //bool InsFirst( ElemType e );
+    //获取循环链表头结点
+    LinkNode<ElemType> *GetHead() { return head; }
+
+    //获取循环链表尾结点
+    LinkNode<ElemType> *GetTail() { return tail; }
+
+    //设置链表头结点
+    void SetHead(LinkNode<ElemType> *p) { head = p; }
+
+    //表尾插入法动态生成链表
+    void CreateList_Tail(int n, ElemType *A);
 
     bool ListTraverse() const;
 };
 
 tmp
-void LinkList<ElemType>::CreateList_Head(int n, ElemType *m) {
+bool CirLinkList<ElemType>::ListTraverse() const {
     Node *p;
-    for (int i = n; i > 0; i--) {
-        p = new Node;
-        p->data = m[i - 1];
-        p->next = head->next;
-        head->next = p;
-    }
-}
-
-tmp
-bool LinkList<ElemType>::ListTraverse() const {
-    Node *next_node;
-    next_node = head->next;
-    while (next_node) {
-        cout << next_node->data << " ";
-        next_node = next_node->next;
+    p = head->next;
+    while (p != head) {
+        cout << p->data << " ";
+        p = p->next;
     }
     cout << endl;
     return true;
 }
 
 tmp
-void Linklist_Contact(LinkList<ElemType> &A, LinkList<ElemType> &B) {
-    Node *next;
-    next = A.GetHead();
-    while (true) {
-        next = next->next;
-        if (next->next == NULL) {
-            next->next = B.GetHead()->next;
-            break;
-        }
+void CirLinkList<ElemType>::CreateList_Tail(int n, ElemType *A) {
+    Node *node, *tmp_node;
+    node = head;
+
+    for (int i = 0; i < n; i++) {
+        tmp_node = new Node;
+        tmp_node->data = A[i];
+
+        node->next = tmp_node;
+        node = tmp_node;
     }
+    node->next = head;
+    tail = node;
+    this->ListTraverse();
 }
 
 tmp
-void Merge_L(LinkList<ElemType> &A, LinkList<ElemType> &B) {
-    Node *real_next1, *real_next2, *node_tmp, *node_tmp2 = NULL;
-    real_next1 = A.GetHead()->next;
-    real_next2 = B.GetHead()->next;
-    while (real_next2 && real_next1) {
-        node_tmp = real_next1->next;
-        real_next1->next = real_next2;
-        if (node_tmp) {
-            node_tmp2 = real_next2->next;
-            real_next2->next = node_tmp;
-        }
-        real_next1 = node_tmp;
-        real_next2 = node_tmp2;
+void Merge_Cur_Linklist(CirLinkList<ElemType> &A, CirLinkList<ElemType> &B) {
+    Node *ah, *at, *bh, *bt;
+    ah = A.GetHead();
+    bh = B.GetHead();
+    at = A.GetTail();
+    bt = B.GetTail();
+    if (bh->next != bh) {
+        at->next = bh->next;
+        bt->next = ah;
+        bh->next = bh;
     }
-}
-
-tmp
-void Merge_L_Order(LinkList<ElemType> &A, LinkList<ElemType> &B) {
-    Node *real_next1, *real_next2, *node_tmp = NULL;
-    real_next1 = A.GetHead()->next;
-    real_next2 = B.GetHead()->next;
-    node_tmp = A.GetHead();
-    while (real_next2 && real_next1) {
-        if (real_next1->data <= real_next2->data) {
-            node_tmp->next = real_next1;
-            node_tmp = real_next1;
-            real_next1 = real_next1->next;
-        } else {
-            node_tmp->next = real_next2;
-            node_tmp = real_next2;
-            real_next2 = real_next2->next;
-        }
-    }
-    if (real_next1) node_tmp->next = real_next1;
-    else node_tmp->next = real_next2;
-}
-
-tmp
-void Purge_Lk_OL(LinkList<ElemType> &A) {
-    Node *head;
-    head = A.GetHead();
-    int k = 0;
-    while (head->next) {
-        if (k == 0) {
-            head = head->next;
-            k++;
-        } else {
-            if (head->data != head->next->data) head = head->next;
-            else head->next = head->next->next;
-        }
-    }
-}
-
-tmp
-void Merge_L_Pure_Order(LinkList<ElemType> &A, LinkList<ElemType> &B) {
-    Merge_L_Order(A, B);
-    Purge_Lk_OL(A);
 }
 
 int main() {
-    int i, n1, n2;
-    LinkList<int> node_list1, node_list2;
-    cin >> n1;//length
+    int n1, n2;
+    CirLinkList<int> list1, list2;
+    cin >> n1;
     int array_list1[n1];
-    for (i = 0; i < n1; i++) cin >> array_list1[i];
-    node_list1.CreateList_Head(n1, array_list1);
-    node_list1.ListTraverse();
+    for (int i = 0; i < n1; i++) cin >> array_list1[i];
+    list1.CreateList_Tail(n1, array_list1);
     cin >> n2;
     int array_list2[n2];
-    for (i = 0; i < n2; i++) cin >> array_list2[i];
-    node_list2.CreateList_Head(n2, array_list2);
-    node_list2.ListTraverse();
-    Merge_L_Pure_Order(node_list1, node_list2);
-    node_list1.ListTraverse();
+    for (int i = 0; i < n2; i++) cin >> array_list2[i];
+    list2.CreateList_Tail(n2, array_list2);
+
+    Merge_Cur_Linklist(list1, list2);
+    list1.ListTraverse();
     return 0;
 }
